020f7f906a70e96c21ca391c95809e16c13a0f6b
Munish <munishrukhaya@gmail.com>
2023-09-26 10:19:11 +0530

    minor changes

--- controllers/postSignup.js
+++ controllers/postSignup.js
@@ -2,17 +2,22 @@ const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
 
 module.exports = async (req, res) => {
-    const existingUser = await User.findOne({ username: req.body.username });
-    if (!existingUser) {
-        const hash = bcrypt.hashSync(req.body.password, 10);
-        User.create({
-            username: req.body.username,
-            email: req.body.email,
-            password: hash
-        }).then((response) => {
-            res.status(200).send({ message: `User with ID:${req.body.username} has been created` });
-        });
-    } else {
-        res.status(401).send({ message: "username already exists" });
+    try {
+        const existingUser = await User.findOne({ username: req.body.username });
+        if (!existingUser) {
+            const hash = bcrypt.hashSync(req.body.password, 10);
+            User.create({
+                username: req.body.username,
+                email: req.body.email,
+                password: hash
+            }).then((response) => {
+                res.status(200).send({ message: `User with ID:${req.body.username} has been created` });
+            });
+        } else {
+            res.status(401).send({ message: "username already exists" });
+        }
+    }
+    catch (err) {
+        res.status(400).send(err);
     }
 }
